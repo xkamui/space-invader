f3e7e17f636cfe8676e1b183dc15556563b656de
Kamui Shirou <x_kamui@msn.com>
2022-03-04 10:41:17 +0100

    Création du 'joueur' dans le canvas.

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const canvas = document.querySelector('#game-container')
 // Change canvas size so it takes full size of the viewport
 setAttributes(canvas, {"width":window.innerWidth, "height":window.innerHeight})
 
+// Get context of the canvas element
+const context = canvas.getContext('2d')
 
+// Create a 'player' in that context
+      context.beginPath()
+      context.arc(canvas.width / 2, canvas.height / 2, 30, 0, Math.PI * 2, false)
+      context.fillStyle = "#CC0000"
+      context.fill()
 
 
 
