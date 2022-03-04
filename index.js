488ff2f2aab53bd7ea274099d0269813d6fd6ce8
Kamui Shirou <x_kamui@msn.com>
2022-03-04 14:12:13 +0100

    Detection des collisions entre les ennemis et les projectiles.

--- index.js
+++ index.js
@@ -1,55 +1,19 @@
+import { Entity } from './modules/classes/classEntity.js'
+import { Player } from './modules/classes/classPlayer.js'
+import { Projectile } from './modules/classes/classProjectile.js'
+import { Enemy } from './modules/classes/classEnemy.js'
+
+// Create canvas, change its size and get context from it
 const canvas = document.querySelector('#game-container')
-
-// Change canvas size so it takes full size of the viewport
 setAttributes(canvas, {"width":window.innerWidth, "height":window.innerHeight})
-
-// Define class Entity
-class Entity {
-    constructor(x, y, radius){
-        this.x = x
-        this.y = y
-        this.radius = radius
-        this.color = '#CC0000'
-    }
-    draw(){
-        context.beginPath()
-        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-        context.fillStyle = this.color
-        context.fill()
-    }
-}
-
-// Define class Player that extends Entity
-class Player extends Entity {
-    constructor(x, y, radius, color){
-        super(x, y, radius)
-        this.color = color
-    }
-}
-
-// Define class projectile that extends from the Player
-class Projectile extends Player {
-    constructor(x, y, radius, color, velocity){
-        super(x, y, radius, color)
-        this.velocity = velocity
-    }
-    update(){
-        this.draw()
-        this.x = this.x + this.velocity.x
-        this.y = this.y + this.velocity.y
-    }
-}
-
-
-// Get context of the canvas element
 const context = canvas.getContext('2d')
 
 // Create a 'player' in that context
-const player = new Player(canvas.width / 2, canvas.height / 2, 10, '#CC0000')
+const player = new Player(context, canvas.width / 2, canvas.height / 2, 10, '#CC0000')
       player.draw()
 
 // Create a projectile
-const projectile = new Projectile(50, 50, 30, '#336699', {x: 3, y:3})
+const projectile = new Projectile(context, 50, 50, 30, '#336699', {x: 3, y:3})
 projectile.draw()
 
 // Create multiple projectiles when the user clicks the window
@@ -62,25 +26,70 @@ window.addEventListener('click', (e) => {
     // Get velocity from the angle
     const velocity = {x: Math.cos(angle) * 5, y: Math.sin(angle) * 5}
 
-    const projectile = new Projectile(e.clientX, e.clientY, 5, '#F5F5F5', velocity)
+    const projectile = new Projectile(context, e.clientX, e.clientY, 5, '#F5F5F5', velocity)
     projectile.draw()
     projectiles.push(projectile)
 })
 
-// Function to add multiple attributes at once
-function setAttributes(obj, attributes){
-    for (key in attributes) {
-        obj.setAttribute(key, attributes[key])
-    }
+// Create multiple enemies with velocity of {x: -1, y: -1}
+const enemies = []
+function spawnEnemies(){
+    setInterval(() => {
+
+        const radius = Math.random() * (30 - 4) + 4
+        
+        const r = Math.floor(Math.random() * 256)
+        const g = Math.floor(Math.random() * 256)
+        const b = Math.floor(Math.random() * 256)
+        const color = `rgb(${r}, ${g}, ${b})`
+    
+        const randomValue = Math.random()
+        let x, y
+        if (randomValue < .25) {
+            x = 0 - radius
+            y = Math.random() * canvas.height
+        } else if (randomValue >= .25 && randomValue < .5) {
+            x = canvas.width + radius
+            y = Math.random() * canvas.height
+        } else if (randomValue >= .5 && randomValue < .75) {
+            x = Math.random() * canvas.width
+            y = 0 - radius
+        } else {
+            x = Math.random() * canvas.width
+            y = canvas.height * radius
+        }
+
+        const angle = Math.atan2(player.y - y, player.x - x)
+        const velocity = {x: Math.cos(angle), y: Math.sin(angle)}
+
+        enemies.push(new Enemy(context, x, y, radius, color, velocity))
+
+    }, 1000)
+
 }
+spawnEnemies()
+
+// Detect collisions between enemies and projectiles
+enemies.forEach((enemy, enemyIndex) => {
+    projectiles.forEach((projectile, projectileIndex) => {
+        const distance = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+        if (distance - projectile.radius - enemies.radius <= 0) {
+            enemies.splice(enemyIndex, 1)
+            projectiles.splice(projectileIndex, 1)
+            console.log('touché')
+        }
+    })
+    enemy.update()
+})
 
 // Function to animate elements on the canvas
 function animate(){
     requestAnimationFrame(animate)
 
-    context.fillStyle = 'rgba(29, 31, 33, 1)'
+    context.fillStyle = 'rgba(29, 31, 33, .1)'
     context.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
     projectiles.map(projectile => projectile.update())
+    enemies.map(enemy => enemy.update())
 }
 animate()
