0ff7cb43dc85594d075871a2b551a4a91ae2ffbc
Kamui Shirou <x_kamui@msn.com>
2022-03-04 10:34:05 +0100

    Manipulation de la taille du canvas.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const canvas = document.querySelector('#game-container')
+
+// Change canvas size so it takes full size of the viewport
+setAttributes(canvas, {"width":window.innerWidth, "height":window.innerHeight})
+
+
+
+
+
+
+// Fonction to add multiple attributes at once
+function setAttributes(obj, attributes){
+    for (key in attributes) {
+        obj.setAttribute(key, attributes[key])
+    }
+}
