439047205fe4067939e9c597d441fd311b930f45
Kamui Shirou <x_kamui@msn.com>
2022-03-04 10:53:53 +0100

    Création des entités via des classes.

--- index.js
+++ index.js
@@ -3,15 +3,38 @@ const canvas = document.querySelector('#game-container')
 // Change canvas size so it takes full size of the viewport
 setAttributes(canvas, {"width":window.innerWidth, "height":window.innerHeight})
 
+// Define class Entity
+class Entity {
+    constructor(x, y, radius){
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = '#CC0000'
+    }
+    draw(){
+        context.beginPath()
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        context.fillStyle = this.color
+        context.fill()
+    }
+}
+
+// Define class Player that extends Entity
+class Player extends Entity {
+    constructor(x, y, radius, color){
+        super(x, y, radius)
+        this.color = color
+    }
+}
+
+
+
 // Get context of the canvas element
 const context = canvas.getContext('2d')
 
 // Create a 'player' in that context
-      context.beginPath()
-      context.arc(canvas.width / 2, canvas.height / 2, 30, 0, Math.PI * 2, false)
-      context.fillStyle = "#CC0000"
-      context.fill()
-
+const player = new Player(canvas.width / 2, canvas.height / 2, 10, '#CC0000')
+      player.draw()
 
 
 
